5d151fe040d14c9f231963343322fd45bd060b7c
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-15 12:28:59 -0600

    first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toBeRemoved) {
+    newArray = [];
+    for(i = 0; i < array.length; i++){
+        if(array[i] != toBeRemoved){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
