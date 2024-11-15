55afed2eab8b988e021ede3c967430c59bb3e65f
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-14 19:38:19 -0600

    closes 02 repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, repeatAmount = 0) {
+    wordWordWord = "";
 
+    for(;repeatAmount > 0; repeatAmount--) {
+        wordWordWord += word;
+    }
+    return wordWordWord;
 };
 
 // Do not edit below this line
