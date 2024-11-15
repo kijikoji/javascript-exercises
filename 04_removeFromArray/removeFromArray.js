90227002cbb691678e018cc30a4d58f35b8244fd
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-15 13:14:16 -0600

    closes_04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,20 @@
 const removeFromArray = function (array) {
     toBeRemoved = Array.prototype.slice.call(arguments, 1);
+    console.log(toBeRemoved);
     newArray = [];
-
     for (i = 0; i < array.length; i++) {
+      console.log(i);
       remove = false;
-
       for (item in toBeRemoved) {
-        if (array[i] == toBeRemoved[item]) {
+        console.log(' array', array[i], 'the item is', toBeRemoved[item]);
+        if (array[i] == toBeRemoved[item] && typeof(array[i]) == typeof(toBeRemoved[item])) {
           remove = true;
         }
       }
-
       if (!remove) {
         newArray.push(array[i]);
       }
     }
-    
     return newArray;
   };
 
