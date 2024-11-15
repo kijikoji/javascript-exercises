a322b033c1414314ad94f29bcf4fe1bfe6531e2a
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-14 19:58:46 -0600

    1 test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    drow = "";
+    text = word.split("");
+    for(i = text.length - 1; i >= 0; i--){
+        drow += text[i];
+    }
+    return drow;
 };
 
 // Do not edit below this line
