9c20fdff8bd0da1f0f5d2c17f150e60b7a06fe4e
kijikoji <cvillagr97@gmail.com>
2024-11-15 18:30:10 -0600

    05 first passed test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    total = 0;
 
+    for(i = num1; i <= num2; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
