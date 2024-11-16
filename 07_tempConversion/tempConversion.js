f57817c5b2f2c69c32b8b2394c87b97ebd6d6320
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-16 17:26:41 -0600

    07 first test pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return round((fahrenheit - 32) * 5 / 9, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return round(celsius * 9 / 5 + 32, 1);
 };
 
 // Do not edit below this line
