b56920ff58434664c18763f1a1e683c7755cb908
kijikoji <cvillagr97@gmail.com>
2024-11-15 18:57:25 -0600

    05 test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,16 @@
-const sumAll = function(num1, num2) {
-    if(num1 < 0 || num2 < 0){
-        return "ERROR";
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || num1 % 1 || num2 % 1) {
+        return 'ERROR';
     }
     total = 0;
-    if(num1 < num2){
-        
-    }else {
+    if (num1 < num2) {
+    } else {
         temp = num1;
         num1 = num2;
         num2 = temp;
     }
 
-    for(i = num1; i <= num2; i++){
+    for (i = num1; i <= num2; i++) {
         total += i;
     }
     return total;
