a5c3363e6a5882956bb017946111eeaa3c056d69
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-19 17:59:12 -0600

    09 test one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const mid = Math.round(word.length / 2,);
+    for (i = 0, j = word.length - 1; i <= mid; i++, j--) {
+        if (word[i] != word[j]) {
+        return false;
+        }
+    }
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
