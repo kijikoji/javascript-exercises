82e7e43e6729d99b5c5cd2b972d49ce44b990b14
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-20 10:20:06 -0600

    09 test three

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (word) {
     word = word.replace(/[.,;:?!-\/]/g, "");
     const mid = Math.round(word.length / 2,);
     for (i = 0, j = word.length - 1; i <= mid; i++, j--) {
-        if (word[i] != word[j]) {
+        if (word[i].toLowerCase() != word[j].toLowerCase()) {
         return false;
         }
     }
