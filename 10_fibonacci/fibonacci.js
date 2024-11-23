5e3cbfa546b9e0330705a4f8f5cc9e6278c997cc
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-23 12:58:38 -0600

    10 first test passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if (pos == 1) {
+        return 1;
+    } else {
+        --pos;
+    }
+    let total = 0;
+    let prev = 0;
+    let next = 1;
+    for (i = 0; i < pos; i ++) {
+        total = next + prev;
+        prev = next;
+        next = total;
+    }
+    return total;
 };
 
 // Do not edit below this line
