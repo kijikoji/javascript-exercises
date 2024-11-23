1c005277382e9dc36781e957b8b6a6ee988c5ab1
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-23 13:55:56 -0600

    12 test one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.reduce((older, people) => {
+        const olderAge = older.yearOfDeath - older.yearOfBirth
+        const peopleAge = people.yearOfDeath - people.yearOfBirth
+        console.log("----------");
+        console.log(older);
+        console.log(people);
 
+        return peopleAge <= olderAge ? older : people;
+    }, {name: "john", yearOfBirth: 0, yearOfDeath: 0});
+    return oldest;
 };
 
 // Do not edit below this line
